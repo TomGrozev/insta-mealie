8425cf17598429ffdd608e14d8489c04d00e5e51
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2026-08-08 19:47:10 +1000

    refactor: format

--- test/insta_mealie_web/controllers/error_html_test.exs
+++ test/insta_mealie_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule InstaMealieWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(InstaMealieWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(InstaMealieWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
